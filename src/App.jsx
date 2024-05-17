d3ddb8dff3c9ceb63142a9665a70afb92dcaf983
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2024-05-17 13:16:48 -0700

    1.9
    Finished 1.9

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import AddToDoForm from './AddToDoForm';
 import TodoList from './TodoList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -13,14 +14,10 @@ function App() {
     const tableName = import.meta.env.VITE_TABLE_NAME;
     const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
     const options = {
-      headers:{Authorization:`Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`}
+      headers: { Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}` }
     }
     try {
-
-      const response = await fetch(url, {
-        method:"GET",
-        headers:{"Authorization":`Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`}
-      });
+      const response = await fetch(url, options);
 
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
@@ -60,11 +57,19 @@ function App() {
   };
 
   return (
-    <>
-      <h1>Todo List</h1>
-      <AddToDoForm onAddTodo={addTodo} />
-      {isLoading ? (<p>Loading..</p>) : (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h1>Todo List</h1>
+            <AddToDoForm onAddTodo={addTodo} />
+            <a href = "/new"> Link to new Page</a>
+            {isLoading ? (<p>Loading..</p>) : (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}
+          </>
+        } />
+        <Route path="/new" element={<h1>New Todo List</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
